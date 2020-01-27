c3bd44eff75a7e648f84ec73b26e8df02054824e
sdttttt <shiinatohka@gmail.com>
2020-01-27 16:28:51 +0800

    Add File Read

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_name = &args[2];
+
+    let contents = fs::read_to_string(file_name).expect("有问题");
+
+    println!("{}", contents);
 }
